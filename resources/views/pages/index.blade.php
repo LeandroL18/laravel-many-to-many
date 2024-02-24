61edb1752a47bfb73a5e12520a75b4e637750782
LeandroL18 <leandrolipani6@gmail.com>
2024-02-24 11:16:46 +0100

    feat: visualizazzione progetti tramite ProjectController

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,12 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Project:</h1>
+    <ol>
+        @foreach ($projects as $project)
+        <li>
+            {{$project -> titolo}}
+        </li>
+        @endforeach
+    </ol>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TypeController;
 
-Route::get('/', function () {
-
-    return view('pages.index');
-});
+Route::get('/', [ProjectController :: class, 'index']) -> name('index');
