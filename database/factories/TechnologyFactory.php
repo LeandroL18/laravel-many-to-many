a63b56ac570972bd801e615c7e91eff94d2ad058
LeandroL18 <leandrolipani6@gmail.com>
2024-02-25 15:07:49 +0100

    feat: factory and seeder technology

--- database/factories/TechnologyFactory.php
+++ database/factories/TechnologyFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Type>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Technology>
  */
-class TypeFactory extends Factory
+class TechnologyFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,7 @@ class TypeFactory extends Factory
     public function definition()
     {
         return [
-            'typology'=>fake()->sentence(3),
+            "name" => fake() -> unique() -> company 
         ];
     }
 }
